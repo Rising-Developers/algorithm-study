d019618cdfa844a97e2fd129c132dd9121352353
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-06-10 21:34:40 +0900

    remove_duplicate_from_sorted_array 문제 해결

--- Leet Code/gihwan/src/remove_duplicate_from_sorted_array/solution.rs
+++ Leet Code/gihwan/src/remove_duplicate_from_sorted_array/solution.rs
@@ -1,1 +1,23 @@
-pub fn solution() {}
+const INVALID: i32 = 1000;
+
+pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    let nums_clone = nums.clone();
+
+    nums.clear();
+
+    let mut count = 0;
+
+    for num in nums_clone {
+        if num != *nums.last().unwrap_or_else(|| &INVALID) {
+            nums.push(num);
+            count += 1;
+        }
+    }
+
+    count
+}
+
+pub fn best_solution_at_leetcode(nums: &mut Vec<i32>) -> i32 {
+    nums.dedup(); // vector 에서 반복되는 값을 제거해주는 메서드
+    nums.len() as i32
+}
