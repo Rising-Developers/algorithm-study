5b5ce2f4640a88df6eb44d375a7b5c7c3351f3e2
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-06-11 15:37:20 +0900

    Leet Code Rotate_array 문제 풀이

--- Leet Code/gihwan/src/main.rs
+++ Leet Code/gihwan/src/main.rs
@@ -1,9 +1,3 @@
 fn main() {
     println!("Hello world");
-
-    let num = vec![2, 2, 1, 1, 1, 2, 2];
-
-    let result = gihwan::majority_element::solution::majority_element(num);
-
-    println!("{}", result);
 }
