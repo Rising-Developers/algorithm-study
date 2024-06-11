c95a2f516f8a36c6064b45ba5a0a66e465273a8b
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-06-11 13:38:33 +0900

    Leet Code remove_duplicate_from_sorted_array_2 문제 풀이

--- Leet Code/gihwan/src/main.rs
+++ Leet Code/gihwan/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello world");
+
+    let mut nums = vec![1];
+
+    let result =
+        gihwan::remove_duplicate_from_sorted_array_2::solution::remove_duplicates(&mut nums);
+
+    println!("{:?}", nums);
+    println!("{}", result);
 }
